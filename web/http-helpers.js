80d92ceeaaf830d026602148ef03b2902c59a442
Hack Reactor Students <uknoiluvu@gmail.com>
2014-03-19 15:47:40 -0700

    Satisfies first test.

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -2,13 +2,19 @@ var path = require('path');
 var fs = require('fs');
 var archive = require('../helpers/archive-helpers');
 
-exports.headers = headers = {
-  "access-control-allow-origin": "*",
-  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "access-control-allow-headers": "content-type, accept",
-  "access-control-max-age": 10, // Seconds.
-  'Content-Type': "text/html"
+exports.sendResponse = function(response, object, status){
+  var headers = {
+    "access-control-allow-origin": "*",
+    "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "access-control-allow-headers": "content-type, accept",
+    "access-control-max-age": 10, // Seconds.
+    'Content-Type': "text/html"
+  };
+  status = status || 200;
+  response.writeHead(status, headers);
+  response.end(JSON.stringify(object));
 };
+
 
 exports.serveAssets = function(res, asset) {
   // Write some code here that helps serve up your static files!
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,13 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var httpHelpers = require('./http-helpers');
+
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  if (req.method === "GET") {
+    if (req.url === "/") {
+      httpHelpers.sendResponse(res, '<input></input>');
+    }
+  }
 };
