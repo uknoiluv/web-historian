cbd0f069e7cf8a8b50684efd87a8e7d02569ef9d
Hack Reactor Students <uknoiluvu@gmail.com>
2014-03-19 17:35:51 -0700

    various changes; non-working

--- web/basic-server.js
+++ web/basic-server.js
@@ -1,9 +1,33 @@
 var http = require("http");
 var handler = require("./request-handler");
+var httpHelpers = require("./http-helpers");
+var url = require('url');
 
 var port = 8080;
 var ip = "127.0.0.1";
-var server = http.createServer(handler.handleRequest);
+
+var routes = {
+  '/': handler.handleRequest
+};
+
+var router = function(req, res) {
+
+  var parsedUri = url.parse(req.url);
+  var path = parsedUri.pathname;
+  if(routes[path] === undefined){
+    routes[path] = handler.handleRequest;
+  }
+
+  var route = routes[path];
+  if (route) {
+    route(req, res);
+  } else {
+    httpHelpers.sendResponse(res, null, 404);
+  }
+
+};
+
+var server = http.createServer(router);
 console.log("Listening on http://" + ip + ":" + port);
 server.listen(port, ip);
 
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,13 +1,27 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
 var httpHelpers = require('./http-helpers');
+var url = require('url');
 
 // require more modules/folders here!
 
+var actions = {
+  'GET': httpHelpers.sendResponse,
+  'POST': function(){},
+  'OPTIONS': function(){}
+};
+
 exports.handleRequest = function (req, res) {
-  if (req.method === "GET") {
-    if (req.url === "/") {
-      httpHelpers.sendResponse(res, '<input></input>');
+
+  var action = actions[req.method];
+  if(action){
+    var name = url.parse(req.url).pathname;
+    if (name === null) {
+      name = '/';
     }
+
+    action(res, name);
+  } else {
+    httpHelpers.sendResponse(res, null, 404);
   }
 };
